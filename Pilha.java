b902142038697dbdc285cd7b94cbb9b8e48212c0
PedroVol <volpe.pedro@escola.pr.gov.br>
2026-06-01 20:32:57 -0300

    Pilha.java

--- Pilha.java
+++ Pilha.java
@@ -2,12 +2,14 @@ public class Pilha {
     String operacao;
     String id;
     int tamanho;
+    No estadoAnterior;
     Pilha proximo;
 
-    public Pilha(String operacao, String id, int tamanho) {
+    public Pilha(String operacao, String id, int tamanho, No estadoAnterior) {
         this.operacao = operacao;
         this.id = id;
         this.tamanho = tamanho;
         this.proximo = null;
+        this.estadoAnterior = estadoAnterior;
     }
 }
